9061577678296815d1d47a63a243f202d812990e
Alex Nichol <unixpickle@gmail.com>
2025-07-26 16:22:26 -0400

    more tree decomp; graph asTree()

--- Sources/LearnGraphs/TreeDecomposition.swift
+++ Sources/LearnGraphs/TreeDecomposition.swift
@@ -17,12 +17,44 @@ public enum TreeDecompositionAlgorithm: Sendable {
 
 extension Graph {
 
+  /// If the graph is a tree, extract the tree from it, optionally specifying
+  /// which node should be the root.
+  public func asTree(root: V? = nil) -> (root: V, children: [V: Set<V>])? {
+    if vertices.isEmpty {
+      return nil
+    }
+    let root = root ?? vertices.first!
+
+    var visited: Set<V> = [root]
+    var children = [V: Set<V>]()
+    var queue = [root]
+    while let curVert = queue.popLast() {
+      for neighbor in neighbors(vertex: curVert) {
+        if visited.contains(neighbor) {
+          if children[neighbor, default: []].contains(curVert) {
+            // This is our parent, it's okay that it's been seen.
+            continue
+          } else {
+            // This is a back-edge to some higher vertex, so there is
+            // a cycle and this is not a tree.
+            return nil
+          }
+        }
+        visited.insert(neighbor)
+        children[curVert, default: []].insert(neighbor)
+        queue.append(neighbor)
+      }
+    }
+
+    return (root: root, children: children)
+  }
+
   public func treeDecomposition(
     algorithm: TreeDecompositionAlgorithm = .arnborg, maxTreewidth: Int? = nil
   ) -> Graph<TreeDecompositionBag<V>>? {
     switch algorithm {
     case .arnborg:
-      for sepSize in 1...(maxTreewidth == nil ? vertices.count : (maxTreewidth! + 1)) {
+      for sepSize in 1...(maxTreewidth == nil ? vertices.count : maxTreewidth!) {
         if let result = arnborgTreeDecomposition(separatorSize: sepSize) {
           return result
         }
@@ -40,10 +72,16 @@ extension Graph {
   }
 
   public func arnborgTreeDecomposition(separatorSize: Int) -> Graph<TreeDecompositionBag<V>>? {
+    if vertices.count <= separatorSize + 1 {
+      return .init(vertices: [.init(bag: vertices)])
+    }
+
     let separators = allSeparators(maxSize: separatorSize)
 
     var sepToClass = [Set<V>: [ArnborgSeparatorClass]]()
-    var clsToTree = [ArnborgSeparatorClass: Graph<TreeDecompositionBag<V>>]()
+    var clsToTree = [
+      ArnborgSeparatorClass: (root: TreeDecompositionBag<V>, graph: Graph<TreeDecompositionBag<V>>)
+    ]()
     var classes = [ArnborgSeparatorClass]()
 
     for sep in separators {
@@ -61,69 +99,77 @@ extension Graph {
     classes.sort { $0.count < $1.count }
 
     for (i, cls) in classes.enumerated() {
-      var found = false
-      if cls.count <= separatorSize {
-        clsToTree[cls] = .init(vertices: [TreeDecompositionBag(bag: cls.vertices)])
-        found = true
+      if cls.count <= separatorSize + 1 {
+        let bag = TreeDecompositionBag(bag: cls.vertices)
+        clsToTree[cls] = (root: bag, graph: .init(vertices: [bag]))
       } else {
-        for v in cls.vertices {
-          // We might end up with some additional v in the child bag
-          // which has some edges connected to cls.separator, in which
-          // case we will need to create an extra bag with these vertices
-          // and with v.
-          let connectedToV = Set(cls.separator.filter { v != $0 && contains(edge: Edge(v, $0)) })
-
-          let superset = cls.separator.union([v])
+        var found = false
+        for v in cls.component {
+          // Find every component that has at most one extra vertex v
+          // from the component of the parent.
+          let allowedSeparator = cls.separator.union([v])
           let relevantClasses = classes[..<i].filter {
             clsToTree[$0] != nil
-              && $0.separator.allSatisfy(superset.contains)
+              && $0.separator.allSatisfy(allowedSeparator.contains)
               && $0.vertices.allSatisfy(cls.component.union($0.separator).contains)
-              && (!$0.separator.contains(v)
-                || $0.separator.union(connectedToV).count <= separatorSize)
           }
-          var classesBySeparator = [Set<V>: [ArnborgSeparatorClass]]()
+          let covered = Set(relevantClasses.flatMap { x in x.vertices })
+          if !cls.component.allSatisfy(covered.contains) {
+            continue
+          }
+
+          // We can form a new tree with v added to the root.
+          found = true
+          let outerRoot = TreeDecompositionBag<V>(bag: cls.separator.union([v]))
+          var tree = Graph<TreeDecompositionBag<V>>(vertices: [outerRoot])
+
+          var alreadyCovered: Set<V> = []
           for cls1 in relevantClasses {
-            classesBySeparator[cls1.separator, default: []].append(cls1)
-          }
-          for (sep, classes) in classesBySeparator {
-            let covered = Set(classes.flatMap { $0.vertices })
-            if cls.component.allSatisfy(covered.contains) {
-              // We have found a new tree.
-              let outerRoot = TreeDecompositionBag<V>(bag: cls.separator)
-              var tree = Graph<TreeDecompositionBag<V>>(vertices: [outerRoot])
-              var connector = outerRoot
-              if sep.contains(v) {
-                let newBag = sep.union(connectedToV)
-                if newBag != cls.separator {
-                  connector = TreeDecompositionBag<V>(bag: newBag)
-                  tree.insert(vertex: connector)
-                  tree.insertEdge(outerRoot, connector)
-                }
+            let (innerRoot, subTree) = clsToTree[cls1]!
+
+            let filteredMapping = Dictionary(
+              uniqueKeysWithValues: subTree.vertices.map { bagV in
+                (bagV, TreeDecompositionBag<V>(bag: bagV.bag.subtracting(alreadyCovered)))
               }
-              for component in classes {
-                let subTree = clsToTree[component]!
-                tree.insert(graph: subTree)
-                let innerRoot = subTree.vertices.filter { sep.allSatisfy($0.bag.contains) }.first!
-                tree.insertEdge(innerRoot, connector)
+            )
+
+            // After filtering, the tree might have been broken up across empty
+            // bags, so we insert each tree separately.
+            let treePieces = subTree.map({ filteredMapping[$0]! }).filteringVertices({
+              !$0.bag.isEmpty
+            }).components()
+            for piece in treePieces {
+              guard let arbitraryVertex = piece.vertices.first else {
+                continue
               }
-              clsToTree[cls] = tree
-              found = true
-              break
+              tree.insert(graph: piece)
+              if piece.vertices.contains(filteredMapping[innerRoot]!) {
+                // The root may have been preserved, so we should connect to it.
+                tree.insertEdge(filteredMapping[innerRoot]!, outerRoot)
+              } else {
+                // This was a separated sub-tree with no vertices in common with
+                // the outer root, so we can connect it at will.
+                tree.insertEdge(arbitraryVertex, outerRoot)
+              }
             }
+
+            alreadyCovered.formUnion(cls1.component)
           }
-          if found {
-            break
-          }
+          clsToTree[cls] = (root: outerRoot, graph: tree)
+          break
+        }
+        if !found {
+          // Skip coverage check for full graph.
+          continue
         }
       }
-      if found && sepToClass[cls.separator]!.allSatisfy({ clsToTree[$0] != nil }) {
+
+      if sepToClass[cls.separator]!.allSatisfy({ clsToTree[$0] != nil }) {
         let outerRoot = TreeDecompositionBag<V>(bag: cls.separator)
         var tree = Graph<TreeDecompositionBag<V>>(vertices: [outerRoot])
         for component in sepToClass[cls.separator]! {
-          let subTree = clsToTree[component]!
+          let (innerRoot, subTree) = clsToTree[component]!
           tree.insert(graph: subTree)
-          let innerRoot = subTree.vertices.filter { cls.separator.allSatisfy($0.bag.contains) }
-            .first!
           tree.insertEdge(innerRoot, outerRoot)
         }
         return tree
--- Tests/LearnGraphsTests/TreeDecomposition.swift
+++ Tests/LearnGraphsTests/TreeDecomposition.swift
@@ -2,9 +2,29 @@ import Testing
 
 @testable import LearnGraphs
 
+@Test
+func testAsTreeCycle() {
+  for n in 3..<6 {
+    var g = Graph(vertices: 0..<n)
+    for i in 0..<n {
+      g.insertEdge(i, (i + 1) % n)
+    }
+    #expect(g.asTree() == nil)
+  }
+}
+
+@Test
+func testAsTreeSmall() {
+  let g = Graph(vertices: 0..<5, edges: [Edge(0, 1), Edge(0, 2), Edge(2, 3), Edge(2, 4)])
+  for v in g.vertices {
+    #expect(g.asTree(root: v) != nil, "unable to build tree at \(v)")
+  }
+  #expect(g.asTree(root: 0)! == (root: 0, children: [0: [1, 2], 2: [3, 4]]))
+}
+
 @Test(arguments: [TreeDecompositionAlgorithm.arnborg])
 func testTreeDecompositionCycle(algorithm: TreeDecompositionAlgorithm) {
-  for n in 3..<6 {
+  for n in 3...6 {
     var g = Graph(vertices: 0..<n)
     for i in 0..<n {
       g.insertEdge(i, (i + 1) % n)
@@ -17,11 +37,56 @@ func testTreeDecompositionCycle(algorithm: TreeDecompositionAlgorithm) {
   }
 }
 
-func getTreeWidth(_ g: Graph<TreeDecompositionBag<Int>>) -> Int {
+@Test(arguments: [TreeDecompositionAlgorithm.arnborg])
+func testTreeDecompositionExample(algorithm: TreeDecompositionAlgorithm) {
+  // https://en.wikipedia.org/wiki/Tree_decomposition#/media/File:Tree_decomposition.svg
+  let g = Graph(
+    vertices: ["A", "B", "C", "D", "E", "F", "G", "H"],
+    edges: [
+      Edge("A", "B"), Edge("A", "C"), Edge("B", "C"), Edge("B", "E"), Edge("B", "G"),
+      Edge("B", "F"), Edge("C", "D"), Edge("C", "E"), Edge("D", "E"), Edge("E", "G"),
+      Edge("E", "H"), Edge("F", "G"), Edge("G", "H"),
+    ]
+  )
+  let maybeTree = g.treeDecomposition(algorithm: algorithm, maxTreewidth: 2)
+  #expect(maybeTree != nil, "tree could not be found")
+  guard let tree = maybeTree else { return }
+  #expect(getTreeWidth(tree) == 2)
+  testValidDecomp(graph: g, tree: tree)
+}
+
+@Test(arguments: [TreeDecompositionAlgorithm.arnborg])
+func testTreeDecompositionGrid(algorithm: TreeDecompositionAlgorithm) {
+  // The grid is:
+  // 0 1 2
+  // 3 4 5
+  // 6 7 8
+  let g = Graph(
+    vertices: 0...8,
+    edges: [
+      // Horizontal
+      Edge(0, 1), Edge(1, 2),
+      Edge(3, 4), Edge(4, 5),
+      Edge(6, 7), Edge(7, 8),
+      // Vertical
+      Edge(0, 3), Edge(3, 6),
+      Edge(1, 4), Edge(4, 7),
+      Edge(2, 5), Edge(5, 8),
+    ]
+  )
+  let maybeTree = g.treeDecomposition(algorithm: algorithm, maxTreewidth: 3)
+  #expect(maybeTree != nil, "tree could not be found")
+  guard let tree = maybeTree else { return }
+  #expect(getTreeWidth(tree) == 3)
+  testValidDecomp(graph: g, tree: tree)
+}
+
+func getTreeWidth<V>(_ g: Graph<TreeDecompositionBag<V>>) -> Int {
   g.vertices.map { $0.bag.count - 1 }.reduce(0, max)
 }
 
-func testValidDecomp(graph g: Graph<Int>, tree decomp: Graph<TreeDecompositionBag<Int>>) {
+func testValidDecomp<V>(graph g: Graph<V>, tree decomp: Graph<TreeDecompositionBag<V>>) {
+  #expect(decomp.asTree() != nil)
   for v in g.vertices {
     #expect(!decomp.vertices.allSatisfy({ !$0.bag.contains(v) }), "vertex \(v) is missing")
   }
